37381666e32dd6549065f21ccab1581913faea08
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2024-11-23 01:00:44 +0300

    change

--- peec/src/components/Hero.jsx
+++ peec/src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
           <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
             <div className="rounded-md shadow">
               <a
-                href="/Contact"
+                href="/contact"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 transition md:py-4 md:text-lg md:px-10"
               >
                 Schedule a Site Visit
